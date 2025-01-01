78ed247079997482b9a9168e28c8f870da114f1c
heywirdgirl <awsmydream@gmail.com>
2025-01-01 12:12:38 +0700

    master

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! 2025</a>
+          Welcome to <a href="https://nextjs.org">Next.js! 2025 MASTER</a>
         </h1>
 
         <p className={styles.description}>
